d895cb8ab35f5444346d00d3278a16ca3a1b67b5
Yoshiki Shibukawa <yoshiki@shibu.jp>
2016-03-20 03:06:24 +0900

    Add MinGW support

--- src/private/endian.h
+++ src/private/endian.h
@@ -2,6 +2,7 @@
 // endian.h
 //
 // https://gist.github.com/panzi/6856583
+// https://albinoloverats.net/src/lmtpd/common/common.h
 //
 // I, Mathias Panzenböck, place this file hereby into the public domain. Use
 // it at your own risk for whatever you like. In case there are
@@ -64,6 +65,73 @@
 
 #	define be64toh(x) betoh64(x)
 #	define le64toh(x) letoh64(x)
+
+#elif (defined(__MINGW32__) || defined(__MINGW64__))
+
+#	define __bswap_16(x) \
+		((((x) & 0xff00) >> 8)\
+		| (((x) & 0x00ff) << 8))
+ 
+#	define __bswap_32(x) \
+		((((x) & 0xff000000) >> 24) \
+		| (((x) & 0x00ff0000) >>  8) \
+		| (((x) & 0x0000ff00) <<  8) \
+		| (((x) & 0x000000ff) << 24))
+ 
+#	define __bswap_64(x) \
+		((((x) & 0xff00000000000000ull) >> 56) \
+		| (((x) & 0x00ff000000000000ull) >> 40) \
+		| (((x) & 0x0000ff0000000000ull) >> 24) \
+		| (((x) & 0x000000ff00000000ull) >> 8)  \
+		| (((x) & 0x00000000ff000000ull) << 8)  \
+		| (((x) & 0x0000000000ff0000ull) << 24) \
+		| (((x) & 0x000000000000ff00ull) << 40) \
+		| (((x) & 0x00000000000000ffull) << 56))
+
+#	if BYTE_ORDER == LITTLE_ENDIAN
+
+#		define htobe16(x) __bswap_16(x)
+#		define htole16(x) (x)
+#		define be16toh(x) __bswap_16(x)
+#		define le16toh(x) (x)
+
+#		define htobe32(x) __bswap_32(x)
+#		define htole32(x) (x)
+#		define be32toh(x) __bswap_32(x)
+#		define le32toh(x) (x)
+
+#		define htobe64(x) __bswap_32(x)
+#		define htole64(x) (x)
+#		define be64toh(x) __bswap_32(x)
+#		define le64toh(x) (x)
+
+#	elif BYTE_ORDER == BIG_ENDIAN
+
+#		define htobe16(x) (x)
+#		define htole16(x) __bswap_16(x)
+#		define be16toh(x) (x)
+#		define le16toh(x) __bswap_16(x)
+
+#		define htobe32(x) (x)
+#		define htole32(x) __bswap_32(x)
+#		define be32toh(x) (x)
+#		define le32toh(x) __bswap_32(x)
+
+#		define htobe64(x) (x)
+#		define htole64(x) __bswap_64(x)
+#		define be64toh(x) (x)
+#		define le64toh(x) __bswap_64(x)
+
+#	else
+
+#		error byte order not supported
+
+#	endif
+
+#	define __BYTE_ORDER    BYTE_ORDER
+#	define __BIG_ENDIAN    BIG_ENDIAN
+#	define __LITTLE_ENDIAN LITTLE_ENDIAN
+#	define __PDP_ENDIAN    PDP_ENDIAN
 
 #elif defined(__WINDOWS__)
 
